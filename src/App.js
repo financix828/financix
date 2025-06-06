9674065883c3ec2d49c7bf182ddb8395e64eabcc
financix828 <financixindia@gmail.com>
2025-06-06 20:02:24 +0530

    Initial Financix website upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Welcome to Financix Consulting</h1>
+      <p>Your global partner for accounting, tax & bookkeeping services.</p>
+    </div>
+  );
+}
+
+export default App;
